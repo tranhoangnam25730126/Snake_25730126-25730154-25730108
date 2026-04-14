868cb56db3b2fe77c3a17782e2a52f74262c9200
Duong-689 <25730108@ms.uit.edu.vn>
2026-04-14 13:20:50 +0700

    add DrawWall function to render walls in the UI

--- UI.cpp
+++ UI.cpp
@@ -9,3 +9,21 @@ void gotoxy(int x, int y)
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+void DrawWall(bool hasWall)
+{
+    if (!hasWall) return;
+    for (int i = 0; i <= WIDTH; i++)
+    {
+        gotoxy(i, 0);
+        cout << "#";
+        gotoxy(i, HEIGHT);
+        cout << "#";
+    }
+    for (int i = 0; i <= HEIGHT; i++)
+    {
+        gotoxy(0, i);
+        cout << "#";
+        gotoxy(WIDTH, i);
+        cout << "#";
+    }
+}
