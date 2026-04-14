abc668940525cc4a903b3959f7bd3a30d3578fc2
Duong-689 <25730108@ms.uit.edu.vn>
2026-04-14 13:10:47 +0700

    add UI.cpp with gotoxy function

---
+++ UI.cpp
@@ -0,0 +1,11 @@
+
+#include "Common.h"
+#include <iostream>
+using namespace std;
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
