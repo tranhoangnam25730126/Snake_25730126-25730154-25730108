d4fabbfb3d78c8d27954fc7525e84a71ae662d06
Duong-689 <25730108@ms.uit.edu.vn>
2026-04-15 12:44:44 +0700

    add DrawWall and DrawSnake functions to render walls and the snake in the UI

--- UI.cpp
+++ UI.cpp
@@ -7,3 +7,29 @@ void gotoxy(int x, int y) {
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+void DrawWall(bool hasWall) {
+    string wallChar = hasWall ? "##" : "::";
+
+    for (int x = 0; x <= WIDTH + 1; x++) {
+        gotoxy(x * 2, 0);
+        cout << wallChar;
+        gotoxy(x * 2, HEIGHT + 1);
+        cout << wallChar;
+    }
+
+    for (int y = 1; y <= HEIGHT; y++) {
+        gotoxy(0, y);
+        cout << wallChar;
+        gotoxy((WIDTH + 1) * 2, y);
+        cout << wallChar;
+    }
+}
+
+void DrawSnake(Snake& s) {
+    for (int i = 0; i < s.body.size(); i++) {
+        gotoxy(s.body[i].X * 2, s.body[i].Y);
+        if (i == 0) cout << "@@";
+        else cout << "()";
+    }
+}
