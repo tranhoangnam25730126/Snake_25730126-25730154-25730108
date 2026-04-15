2456336e6f6d4184986912f1c1eb1f419b813cc8
Tran Hoang Nam - 25730126@ms.uit.edu.vn <25730126@ms.uit.edu.vn>
2026-04-15 14:24:10 +0700

    khoi tao bien

--- Snake.cpp
+++ Snake.cpp
@@ -6,5 +6,21 @@
 
 bool isGameOver = false;
 
-int main()
-{}
+int main(){
+
+    srand((unsigned int)time(NULL));
+
+    Snake s;
+    Food f;
+
+    bool isKeepPlaying = true;
+    while (isKeepPlaying) {
+        ShowMenu();
+        char choice = _getch();
+        while (choice != '1' && choice != '2')
+            choice = _getch();
+
+        if (choice == '1') hasWall = true;
+        else hasWall = false;}
+
+}
