268620da19dfb7536d2fb12d21590a7e96aca8cf
Tran Hoang Nam - 25730126@ms.uit.edu.vn <25730126@ms.uit.edu.vn>
2026-04-15 14:32:35 +0700

    hoan thien ham main

--- Snake.cpp
+++ Snake.cpp
@@ -14,6 +14,7 @@ int main(){
     Food f;
 
     bool isKeepPlaying = true;
+
     while (isKeepPlaying) {
         ShowMenu();
         char choice = _getch();
@@ -21,6 +22,65 @@ int main(){
             choice = _getch();
 
         if (choice == '1') hasWall = true;
-        else hasWall = false;}
+        else hasWall = false;
 
+        system("cls");
+        Setup(s);
+        DrawWall(hasWall);
+        DrawSnake(s);
+        GenerateFood(f, s);
+        DrawFood(f);
+        score = 0;
+        DrawScore(score);
+        isGameOver = false;
+        isPaused = false;
+
+        while (!isGameOver) {
+            Input(s);
+            if (!isPaused) {
+            Move(s);
+
+                if (CheckEat(s, f)) {
+                    Beep(800, 50);
+                    GenerateFood(f, s);
+                    DrawFood(f);
+                    DrawScore(score);
+                }
+
+            if (isCollision(s)) {
+                    isGameOver = true;
+                    break;
+                }
+
+                DrawSnake(s);
+
+                Sleep(100);
+            }
+        }
+
+        gotoxy(0, HEIGHT + 2);
+        cout << "===========================" << endl;
+        cout << "   GAME OVER! SCORE: " << score << endl;
+        cout << "===========================" << endl;
+
+        gotoxy(0, HEIGHT + 5);
+        cout << "Press 'R' to Play Again or 'X' to Exit!";
+
+        bool waitingForInput = true;
+        while (waitingForInput) {
+            char choice1 = _getch();
+            if (choice1 == 'r' || choice1 == 'R') {
+                waitingForInput = false;
+            }
+            else if (choice1 == 'x' || choice1 == 'X') {
+                isKeepPlaying = false;
+                waitingForInput = false;
+            }
+        }
+    }
+
+    gotoxy(0, HEIGHT + 6);
+    cout << "Press any key to exit...";
+
+    _getch();
 }
